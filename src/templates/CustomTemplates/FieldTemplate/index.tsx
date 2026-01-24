69400ef60fde75ab4cabc9dfc94e52932ecf65e3
David Way <david_way@live.ie>
2026-01-24 16:14:59 +0000

    refactor: template imports

---
+++ src/templates/CustomTemplates/FieldTemplate/index.tsx
@@ -0,0 +1,29 @@
+import { Label, Stack } from "@components";
+import type { FieldTemplateProps } from "@rjsf/utils";
+
+export default function FieldTemplate(props: FieldTemplateProps) {
+  const {
+    id,
+    classNames,
+    style,
+    label,
+    help,
+    required,
+    description,
+    errors,
+    children,
+  } = props;
+
+  console.log(">> field", props);
+  return (
+    <Stack spacing="16" className={classNames} style={style}>
+      <Label htmlFor={id} required={required}>
+        {label}
+      </Label>
+      {/* {description} */}
+      {children}
+      {errors}
+      {help}
+    </Stack>
+  );
+}
